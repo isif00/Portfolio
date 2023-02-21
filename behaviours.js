14aeaa49ff8b89149b5f3e58edae59beabdc7a86
isif00 <sifodev@gmail.com>
2023-02-21 12:02:04 +0100

    Fixing Skills menu Isuues

--- behaviours.js
+++ behaviours.js
@@ -1,17 +1,3 @@
-var menuIcon = document.getElementById("menuIcon")
-var sideNav = document.getElementById("sideNav")
-var menu = document.getElementById("menu")
-
-menuIcon.onclick = function(){
-    if(sideNav.style.right == "-250px")
-    {
-        sideNav.style.right = "0";
-    }
-    else{
-        sideNav.style.right = "-250px";
-    }
-}
-
 document.getElementById("skills").addEventListener("click", function() {
     window.scrollBy({
         top: window.innerHeight,
