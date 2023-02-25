ca5f40582ef0a065a32dc5765dd9700434117d19
isif00 <sifodev@gmail.com>
2023-02-25 15:47:16 +0100

    Fixing Secuirty Issues

--- ProjectManager/Manager/models.py
+++ ProjectManager/Manager/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Manager(models.Model):
+    title = models.CharField(max_length=250)
+    image = models.ImageField(upload_to="Images")
+    link = models.CharField(max_length=250)
+    publish_date = models.DateField()
+
+def __str__(self):
+    return self.name
