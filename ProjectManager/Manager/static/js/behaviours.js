ed52cd64f853e99fb69436781f1d4d2f5cd4091f
isif00 <sifodev@gmail.com>
2023-03-01 00:03:37 +0100

    Fixing Bugs & adding footer

--- ProjectManager/Manager/static/js/behaviours.js
+++ ProjectManager/Manager/static/js/behaviours.js
@@ -10,8 +10,9 @@ const sectionHeight = document.querySelector('section').clientHeight;
 
 document.getElementById("projects").addEventListener("click", function() {
     window.scrollBy({
-        top: sectionHeight * 1.99,
+        top: sectionHeight * -2,
         behavior: "smooth"
     })
     
 })
+
--- ProjectManager/ProjectManager/settings.py
+++ ProjectManager/ProjectManager/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'ProjectManager.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates', 'static')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates','static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +122,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+print(BASE_DIR)
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
