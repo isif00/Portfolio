6b135176a57b54978d253ab2a9608a80812b8a0a
isif00 <sifodev@gmail.com>
2023-03-22 23:19:07 +0100

    Fixing issues

--- ProjectManager/ProjectManager/settings.py
+++ ProjectManager/ProjectManager/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOST = ['salmisifeddine.up.railway.app']
+ALLOWED_HOSTS = ['salmisifeddine.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://salmisifeddine.up.railway.app']
 
 # Application definition
