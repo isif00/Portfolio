3f671cff89b064835bb42ae4b379dd08d79f8478
isif00 <sifodev@gmail.com>
2023-03-24 02:29:22 +0100

    Fixing Deployements  settings

--- ProjectManager/ProjectManager/settings.py
+++ ProjectManager/ProjectManager/settings.py
@@ -11,12 +11,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os 
 import dj_database_url
-
+import django
 from pathlib import Path
 from dotenv import load_dotenv
 
 
 load_dotenv()
+django.setup()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
